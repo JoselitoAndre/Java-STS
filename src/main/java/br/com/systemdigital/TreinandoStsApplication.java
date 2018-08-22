0b1923a48ec9387bfecd4f2544579cae23ec1a15
JoselitoAndre <jandrearaujo@bol.com.br>
2018-08-22 15:27:47 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/systemdigital/TreinandoStsApplication.java
@@ -0,0 +1,12 @@
+package br.com.systemdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinandoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinandoStsApplication.class, args);
+	}
+}
