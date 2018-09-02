24e6155fb860c4a999b39ede3b3bd451c460d819
JoselitoAndre <jandrearaujo@bol.com.br>
2018-09-02 05:55:09 -0300

    Criando operação de instanciação

--- src/main/java/br/com/systemdigital/TreinandoStsApplication.java
+++ src/main/java/br/com/systemdigital/TreinandoStsApplication.java
@@ -1,12 +1,32 @@
 package br.com.systemdigital;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TreinandoStsApplication {
+import br.com.systemdigital.domain.Categoria;
+import br.com.systemdigital.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class TreinandoStsApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(TreinandoStsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria( null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
