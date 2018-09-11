5a66d98f0e46d44f11f4175658801b44fd6c7a63
JoselitoAndre <jandrearaujo@bol.com.br>
2018-09-11 16:07:30 -0300

    Ajstes no endpoint/{id}

--- src/main/java/br/com/systemdigital/services/CategoriaService.java
+++ src/main/java/br/com/systemdigital/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.systemdigital.domain.Categoria;
 import br.com.systemdigital.repositories.CategoriaRepository;
+import br.com.systemdigital.services.exceptions.ObjectNotFoundException;
 
 @Service 
 public class CategoriaService {
@@ -15,7 +16,11 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id); 
-		return obj.orElse(null);
+		/*Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null);*/
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
