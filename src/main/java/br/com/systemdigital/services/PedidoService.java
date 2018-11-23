8b25214b6f7fd51ac939d50d06cca2b42af5bd12
JoselitoAndre <jandrearaujo@bol.com.br>
2018-11-23 19:47:10 -0300

    Endpoint /pedidp/{id} disponibilizado

---
+++ src/main/java/br/com/systemdigital/services/PedidoService.java
@@ -0,0 +1,27 @@
+
+package br.com.systemdigital.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.systemdigital.domain.Pedido;
+import br.com.systemdigital.repositories.PedidoRepository;
+import br.com.systemdigital.services.exceptions.ObjectNotFoundException;
+
+@Service 
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		/*Optional<Pedido> obj = repo.findById(id); 
+		return obj.orElse(null);*/
+		
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
